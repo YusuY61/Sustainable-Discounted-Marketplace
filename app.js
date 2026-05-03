fdfd257235fd6b1ad53b26d4ec2faeea60d6b86b
adilerena <aerenaydgn@gmail.com>
2026-05-03 17:40:12 +0300

    pusshh

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 import express from "express";
+import session from "express-session";
+import db from "./db.js";
+import "dotenv/config";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.get("/", async (req, res) => {
+    res.render("index");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Running on port ${process.env.PORT}`);
+});
